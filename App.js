0dd334d42fd32faa4320a92fd4358381d9a8950e
Akshay Kannan <akshayk@akshayk-macbookpro3.roam.corp.google.com>
2019-06-01 13:42:26 -0700

    lots of stuff

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-import { AsyncStorage, Button, Dimensions, Keyboard, Platform, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
+import { AsyncStorage, Button, Dimensions, Keyboard, Platform, Share, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
 import { TabView, SceneMap } from 'react-native-tab-view';
 import { ConfirmDialog } from 'react-native-simple-dialogs';
 
@@ -19,21 +19,28 @@ class Tab extends React.Component {
   static propTypes = {
     routeKey: PropTypes.number.isRequired,
     onChangeText: PropTypes.func.isRequired,
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    bottomOffset: PropTypes.number.isRequired
   }
 
   render() {
+    let lines = this.props.text.split('\n');
+    let capitalize = lines.length > 0 && lines[lines.length - 1].startsWith('• ') && lines[lines.length - 1].length < 3;
+
     backgroundColor = ((this.props.routeKey % 2) == 0) ? '#555' : '#444';
     return (
       <View style={[styles.scene, {backgroundColor: backgroundColor}]}>
         <TextInput 
-          style={{color: '#FFF', height: '100%', fontSize: 32, textAlignVertical: 'top', padding: 10}}
+          style={{color: '#FAFAFA', fontSize: 22, textAlignVertical: 'top', padding: 10,
+            height: (Dimensions.get('window').height - 150 - this.props.bottomOffset)}}
           multiline={true}
+          autoCorrect={true}
           ref={(input) => this.input = input}
           placeholder={this.state.placeholderText}
           onFocus={() => this.setState({placeholderText: ''})}
           onBlur={() => this.setState({placeholderText: placeholderText})}
           autoFocus={true}
+          autoCapitalize={capitalize ? 'words' : 'sentences'}
           value={this.props.text}
           onChangeText={(text) => this.props.onChangeText(this.props.routeKey, text)}
           />
@@ -66,8 +73,20 @@ export default class App extends React.Component {
       routeKey={key}
       text={this.state.notes[key]}
       ref={(tab) => this.tabs[key] = tab}
+      bottomOffset={this.state.bottomOffset}
       onChangeText={ (key, content) => {
-        //TODO: Don't edit this directly- make a copy
+        // If last char was newline and previous line starts with bullet, continue the list
+        if (content[content.length - 1] === '\n' && (content.length > this.state.notes[key].length)) {
+          let lines = content.split('\n');
+          if (lines.length > 1 && (lines[lines.length - 2][0] === '•')) {
+            if (lines[lines.length - 2].length > 2) this.setState(this.addBulletToCurrentNote);
+            else if (lines[lines.length - 2].length === 2) this.setState(this.clearLastLine);
+            return;
+          }
+        }
+
+        // Save state
+        // TODO: Don't edit this directly- make a copy
         let notes = this.state.notes;
         let routes = this.state.routes;
         var index;
@@ -77,7 +96,7 @@ export default class App extends React.Component {
         routes[index].title = displayTitle(content);
         this.setState({
           notes: {...notes, [key]: content},
-          routes: routes
+          routes: routes,
         });
       }}
       />;
@@ -157,12 +176,44 @@ export default class App extends React.Component {
     })
   }
 
+  async share() {
+    let key = this.state.routes[this.state.index].key;
+    try {
+      const result = await Share.share({
+        message: this.state.notes[key],
+      })
+
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  clearLastLine(prevState) {
+    let key = this.state.routes[this.state.index].key;
+    let lines = prevState.notes[key].split('\n');
+    if (lines.length > 1) lines[lines.length - 1] = '';
+    return ({
+      notes: {...prevState.notes, [key]: lines.join('\n')}
+    });
+  }
+
   addBulletToCurrentNote(prevState) {
     let key = this.state.routes[this.state.index].key;
     let lines = prevState.notes[key].split('\n');
     let needsNewline = lines[lines.length - 1].trim() !== '';
     return ({
-      notes: {...prevState.notes, [key]: (prevState.notes[key] + '• ')}
+      notes: {...prevState.notes, [key]: (prevState.notes[key]
+        + (needsNewline ? '\n' : '')
+        + '• ')}
     });
   }
 
@@ -185,7 +236,7 @@ export default class App extends React.Component {
           negativeButton={{title: "Cancel", onPress: () => this.setState({deleteDialog: false}) }}
       />
 
-        <View style={{position: 'absolute', bottom: 5 + this.state.bottomOffset, width: '100%'}}>
+        <View style={{position: 'absolute', bottom: this.state.bottomOffset, width: '100%', margin: 5}}>
           <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', position: 'relative'}}>
             <Icon.Button name="md-trash" onPress={() => this.setState({deleteDialog: true})}
               style={[styles.actionButtonIcon, styles.large, {backgroundColor: '#AA3333'}]} />
@@ -196,7 +247,7 @@ export default class App extends React.Component {
             <Icon.Button name="md-camera" onPress={() => alert('Camera input not implemented')}
               style={styles.actionButtonIcon, styles.large} />
 
-            <Icon.Button name="ios-share" onPress={() => alert('Share not implemented')}
+            <Icon.Button name="ios-share" onPress={() => this.share()}
               style={[styles.actionButtonIcon, styles.large, {backgroundColor: '#33AA33'}]} />
           </View>
         </View>
